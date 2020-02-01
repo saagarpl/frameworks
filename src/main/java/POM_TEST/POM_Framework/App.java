a1608041fc04d7ecb234e02b58e8fe73af2430ef
Sagar Ladwa <Sagar.Ladwa@causeway.com>
2020-02-02 01:15:51 +0530

    New test pom project

---
+++ src/main/java/POM_TEST/POM_Framework/App.java
@@ -0,0 +1,13 @@
+package POM_TEST.POM_Framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
